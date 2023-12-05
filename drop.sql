d2b398eb835f21eb759940b613fc0d47953c2328
angelo <angelogpradella@gmail.com>
2023-12-05 14:14:38 +0100

    correzione forse giusta cambio credenziali db

--- drop.sql
+++ drop.sql
@@ -906,3 +906,43 @@
     drop table if exists libro;
 
     drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
+
+    drop table if exists libro;
+
+    drop table if exists utente;
